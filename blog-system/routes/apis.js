cd46697dffd9f1b18e16e3c95e082edbd5b472a3
thousand0001 <thousand0001@gmail.com>
2017-10-21 21:28:56 +0800

    init

--- blog-system/routes/apis.js
+++ blog-system/routes/apis.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+// test github
 /* 使用者刪除文章功能. */
 router.get('/delete/:id', function(req, res, next) {
   res.send("This is the delete function.");
